9f2ed92a860d0cd9ea20928cc9959aa5d8d7ed89
couyang24 <30837746+couyang24@users.noreply.github.com>
2018-05-10 15:39:06 -0400

    update

---
+++ draft.R
@@ -0,0 +1,22 @@
+library(wordcloud2)
+wordcloud2(data = demoFreq)
+
+require(devtools)
+install_github("lchiffon/wordcloud2")
+
+figPath = system.file("examples/t.png",package = "wordcloud2")
+wordcloud2(demoFreq, figPath = figPath, size = 1.5,color = "skyblue")
+
+letterCloud( demoFreq, word = "R", color='random-light' , backgroundColor="black")
+
+letterCloud( demoFreq, word = "PEACE", color="white", backgroundColor="pink")
+wordcloud2(demoFreq, size=1.6)
+wordcloud2(demoFreq, size=1.6, color='random-dark')
+wordcloud2(demoFreq, size=1.6, color=rep_len( c("green","blue"), nrow(demoFreq) ) )
+wordcloud2(demoFreq, size=1.6, color='random-light', backgroundColor="black")
+wordcloud2(demoFreq, size = 0.7, shape = 'star')
+wordcloud2(demoFreq, figPath = "C:/Users/Owen/Projects/Star-Wars-Movie-Scripts/yoda.png", 
+           size = 1.5, color = "skyblue", backgroundColor="black")
+
+
+wordcloud2(demoFreq, figPath = "C:/Users/Owen/Projects/Star-Wars-Movie-Scripts/yoda.png", size = 1.5,color = "skyblue")
---
+++ draft2.R
@@ -0,0 +1,57 @@
+pacman::p_load(tidyverse, tm, wordcloud, wordcloud2, tidytext, reshape2)
+
+# Read the data
+ep4 <- read.table("SW_EpisodeIV.txt")
+ep5 <- read.table("SW_EpisodeV.txt")
+ep6 <- read.table("SW_EpisodeVI.txt")
+
+
+
+# Wordcloud for Episode V
+cleanCorpus <- function(corpus){
+  
+  corpus.tmp <- tm_map(corpus, removePunctuation)
+  corpus.tmp <- tm_map(corpus.tmp, stripWhitespace)
+  corpus.tmp <- tm_map(corpus.tmp, content_transformer(tolower))
+  v_stopwords <- c(stopwords("english"), c("thats","weve","hes","theres","ive",
+                                           "will","can","cant","dont","youve",
+                                           "youre","youll","theyre","whats","didnt","us"))
+  corpus.tmp <- tm_map(corpus.tmp, removeWords, v_stopwords)
+  corpus.tmp <- tm_map(corpus.tmp, removeNumbers)
+  return(corpus.tmp)
+  
+}
+
+
+frequentTerms <- function(text){
+  
+  s.cor <- Corpus(VectorSource(text))
+  s.cor.cl <- cleanCorpus(s.cor)
+  s.tdm <- TermDocumentMatrix(s.cor.cl)
+  s.tdm <- removeSparseTerms(s.tdm, 0.99)
+  m <- as.matrix(s.tdm)
+  word_freqs <- sort(rowSums(m), decreasing=TRUE)
+  dm <- data.frame(word=names(word_freqs), freq=word_freqs)
+  return(dm)
+  
+}
+
+
+combined <- frequentTerms(ep6$dialogue) %>% bind_rows(frequentTerms(ep6$dialogue)) %>% 
+    bind_rows(frequentTerms(ep4$dialogue))
+
+combined <- combined %>% mutate(freq = if_else(freq>35, freq+25, freq)) %>% arrange(desc(freq))
+
+wordcloud2(combined, size=.5,
+           figPath="vader.png")
+
+
+# ep5$dialogue %>% VectorSource() %>% Corpus() %>% cleanCorpus() %>% 
+#   TermDocumentMatrix() %>% removeSparseTerms(0.99) %>% as.matrix() %>% rowSums() %>% 
+#   sort( decreasing=TRUE)
+
+wordcloud2(combined,size=0.55,
+           figPath="yoda.png")
+
+# wordcloud2(demoFreq, 
+#            figPath = "C:/Users/Owen/Projects/Star-Wars-Movie-Scripts/yoda.png", size = 1.5,color = "skyblue")
